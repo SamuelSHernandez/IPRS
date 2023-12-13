6e1abc7383f5052578da477e0805824e6f60c8af
Samuel Hernandez <HernandezS@gc.adventist.org>
2023-12-13 10:30:52 -0500

    Create conference.csv

--- Data Analysis/get_conferences.py
+++ Data Analysis/get_conferences.py
@@ -3,7 +3,7 @@ import json
 import requests
 from collections import defaultdict
 import logging as log
-from DATA import api_url
+from DATA import api_url, union_dict
 
 CSV_FILE_PATH = "conference.csv"
 FIELDNAMES = ['id', 'union_id', 'name']
@@ -47,18 +47,16 @@ def group_by_id(entries, id_key):
     return grouped_data
 
 def get_conferences(api_url, csv_file_path):
-    """
-    Main function to fetch data from the API, group it, and export to CSV.
-
-    Args:
-        api_url (str): URL of the API.
-        csv_file_path (str): Path to the CSV file.
-    """
     try:
         data = fetch_data_from_api(api_url)
         if data:
-            union_id_group = group_by_id(data, "UnionID")
-            export_to_csv(union_id_group, csv_file_path)
+            conference_dict = defaultdict(list)
+            for _child in data:
+                parent_code = _child.get("ParentID")
+                if any(parent_code == code for name, code in union_dict.values()):
+                    parent_id = next(key for key, (name, code) in union_dict.items() if code == parent_code)
+                    conference_dict[parent_id].append(_child)
+            export_to_csv(conference_dict, csv_file_path)
             log.info(f"CSV file '{csv_file_path}' created successfully.")
         else:
             log.info("No data fetched from the API.")
@@ -80,14 +78,15 @@ def export_to_csv(data, csv_file_path):
     with open(csv_file_path, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
         writer.writeheader()
-        for _id, (identifier, entries) in enumerate(data.items()):
-            for entry in entries:
-                entry['ID'] = _id
+        id_counter = 0
+        for parent_id, child_entries in data.items():
+            for _, child_entry in enumerate(child_entries):
                 writer.writerow({
-                    'id': entry.get('CustomID', ''),
-                    'name': entry.get('Name', ''),
-                    'union_id': entry.get('UnionID', ''),
+                    'id': id_counter,
+                    'name': child_entry.get('Name', ''),
+                    'union_id': parent_id,
                 })
+                id_counter+=1
 
 
 if __name__ == "__main__":
